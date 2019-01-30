983b343d06dc33b697493e0d4f65e3c53b929f6b
Kireei <bjerned@gmail.com>
2019-01-30 19:37:05 +0100

    Test fråån bärbar

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -51,7 +51,7 @@ public class MainGameLoop {
 		
 		UIMaster.init(loader);
 		//UIMaster.loadUI(new UIElement(new Vector2f(0,0), new Vector2f(1f, 1f), "Escape Menu"));
-		
+		//Test för github
 		TextMaster.init(loader);
 		
 		
